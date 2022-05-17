bb93560f6945be6bd2dfd82af7d8f50283f5e0b8
justinkim2015 <justinkim2015@gmail.com>
2022-05-17 13:33:43 +0900

    Write methods for linked list

--- main.rb
+++ main.rb
@@ -1,7 +1,5 @@
 # Full linked list
 class LinkedList
-  attr_accessor :head, :tail
-
   def initialize
     @head = nil
     @tail = nil
@@ -15,13 +13,58 @@ class LinkedList
       current = @head
       current = current.next until current.next.nil?
       current.next = this_node
+      @tail = this_node
     end
   end
 
   def prepend(value)
     this_node = Node.new(value)
-    this_node.next = @head unless @head.nil?
-    @head = this_node
+    if @head.nil?
+      @head = this_node
+      @tail = this_node
+    else
+      this_node.next = @head
+      @head = this_node
+    end
+  end
+
+  def size
+    current = @head
+    count = 1
+    until current.next.nil?
+      count += 1
+      current = current.next
+    end
+    puts "There are currently #{count} nodes"
+  end
+
+  def head
+    @head.value
+  end
+
+  def tail
+    @tail.value
+  end
+
+  def at(index)
+    current = @head
+    i = 0
+    unless index.zero?
+      until i == index
+        current = current.next
+        i += 1
+      end
+    end
+    current.value
+  end
+
+  def to_s
+    current = @head
+    until current.next.nil?
+      print "( #{current.value} ) -> "
+      current = current.next
+    end
+    print "( #{@tail.value} ) "
   end
 end
 
@@ -36,6 +79,8 @@ class Node
 end
 
 list = LinkedList.new
-5.times do
-  list.prepend(rand(10))
+10.times do
+  list.append(rand(10))
 end
+
+list.at(0)
